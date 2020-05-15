8a89b55411c4ef3cf05486a0c32856f51ae1b58b
quangdat99 <dat21051999@gmail.com>
2020-05-15 21:28:17 +0700

    ahihi

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+// var cookieParser = require('cookie-parser');
+// var mongoose = require('mongoose');
+
+var port = 3001;
+
+var app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser("abc123xyz"));
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
+
+app.listen(port, function () {
+	console.log('Server listening on port '+ port);
+});
