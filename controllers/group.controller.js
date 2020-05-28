55a7f8cad4719e427f68a9f93958040fd0853f67
doannd1997 <duydoan0907@gmail.com>
2020-05-28 16:34:15 +0700

    Create Group

--- controllers/group.controller.js
+++ controllers/group.controller.js
@@ -2,6 +2,7 @@ var User = require('../models/user.model');
 var List = require('../models/list.model');
 var Board = require('../models/board.model');
 var Group = require('../models/group.model');
+var USer_Group = require('../models/user_group.model');
 var Recent = require("../models/recent.model");
 const {BOARD_TYPE, MAX_RECENT} = require("./const/Const");
 
@@ -42,6 +43,8 @@ module.exports.create = async (req, res)=>{
 	var description = req.body.description;
 	var memberCount = 0;
 
+	var userId = req.signedCookies.userId;
+
 	var group = new Group({
 		title: title,
 		description: description,
@@ -49,39 +52,15 @@ module.exports.create = async (req, res)=>{
 	})
 
 	try {
-		await group.save();
-		var user = await User.findOne({_id: req.signedCookies.userId});
-		// private board
-		var privateBoards = await Board.find({boardType: BOARD_TYPE.PRIVATE, userId: user._id});
-		// group & shared-board
-		var groups = await Group.find({_id: user.groupId});
-		var sharedBoards = [];
-		for (var g in groups){
-			var group = groups[g];
-			var groupBoard = await Board.find({boardType: BOARD_TYPE.SHARED, groupId: group._id});
-			var sBoard = {
-				groupId: group._id,
-				boards: JSON.parse(JSON.stringify(groupBoard))
-			};
-			sharedBoards.push(sBoard);
-		}
-		var recents = await Recent.find({userId: user._id});
-		recents.sort((r0, r1)=>{
-			return r1.timeVisited - r0.timeVisited
-		});
-		var slicer = Math.min(recents.length, MAX_RECENT);
-		recents = recents.slice(0, slicer);
+		group.save();
 
-		// render
-		res.render("home", {
-			err: "none",
-			value: {
-				privateBoards: privateBoards,
-				sharedBoards: sharedBoards,
-				groups: groups,
-				recents: recents,
-			},
+		var user_group = new USer_Group({
+			userId: userId,
+			groupId: group._id
 		});
+		await user_group.save();
+
+		res.redirect("/home");
 	}
 	catch (e){
 		console.log(e)
