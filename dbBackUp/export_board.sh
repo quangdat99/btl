a3b3455063d781c264e91b33167a75127b989393
quangdat99 <dat21051999@gmail.com>
2020-06-04 15:58:51 +0700

    backup db

---
+++ dbBackUp/export_board.sh
@@ -0,0 +1,2 @@
+mongoexport --uri="mongodb://localhost:27017/work_management"  --collection=board  --out=board.json
+pause
---
+++ dbBackUp/import_group.sh
@@ -0,0 +1,2 @@
+mongoimport --uri="mongodb://localhost:27017/work_management"  --collection=group  --file=./group.json --type=json --mode=upsert
+pause
