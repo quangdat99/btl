ffc3b7b5bc93587240e8a53c126e879949e1e6d4
doannd1997 <duydoan0907@gmail.com>
2020-06-05 10:40:00 +0700

    Add member

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -13,23 +13,23 @@ const filterUser = (users, field)=>{
 // tìm user không trong nhóm này để thêm vào nhóm group
 // tìm partners =>> trả về những ai không phải parter
 module.exports.postSearchAllUser = function(req, res) {
-	var field= new RegExp(req.query["term"],'i');
- 
-  var users =User.find({displayName:field},{'displayName':1}).sort({"updated_at":-1}).sort({"created_at":-1}).limit(20);
-  users.exec(function(err,data){
+	var field = new RegExp(req.query["term"], "i");
 
-		var result=[];
-		if(!err){
-			if(data && data.length && data.length>0){
-				data.forEach(user=>{
-				 result.push(user.displayName);
-			 });
-
-			}
-			res.jsonp(result);
-		}
-
-	});
+  var users = User.find({ displayName: field }, { displayName: 1 })
+    .sort({ updated_at: -1 })
+    .sort({ created_at: -1 })
+    .limit(20);
+  users.exec(function (err, data) {
+    var result = [];
+    if (!err) {
+      if (data && data.length && data.length > 0) {
+        data.forEach((user) => {
+          result.push(user.displayName);
+        });
+      }
+      res.jsonp(result);
+    }
+  });
 };
 
 // tìm user trong group để chỉ định công việc
