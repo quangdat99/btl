64dfdec5540ee43e2808227e5965f55480cc024d
quangdat99 <dat21051999@gmail.com>
2020-06-05 10:16:43 +0700

    search

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -4,66 +4,66 @@ var User_Group = require("../models/user_group.model");
 
 
 const filterUser = (users, field)=>{
-	var reg = new RegExp(field, "gi")
-	return users.filter((user)=>{
-		return (user.email.search(reg) != -1 || user.displayName.search(reg) != -1);
-	});
+  var reg = new RegExp(field, "gi")
+  return users.filter((user)=>{
+    return (user.email.search(reg) != -1 || user.displayName.search(reg) != -1);
+  });
 }
 
 // tìm user không trong nhóm này để thêm vào nhóm group
 // tìm partners =>> trả về những ai không phải parter
-module.exports.postSearchAllUser = function(req, res) {
-	var field= new RegExp(req.query["term"],'i');
- 
+module.exports.postSearchAllUser = function(req, res, next ) {
+  var field= new RegExp(req.query["term"],'i');
+  console.log(req.body.name);
   var users =User.find({displayName:field},{'displayName':1}).sort({"updated_at":-1}).sort({"created_at":-1}).limit(20);
   users.exec(function(err,data){
 
-		var result=[];
-		if(!err){
-			if(data && data.length && data.length>0){
-				data.forEach(user=>{
-				 result.push(user.displayName);
-			 });
+    var result=[];
+    if(!err){
+      if(data && data.length && data.length>0){
+        data.forEach(user=>{
+         result.push(user.displayName);
+       });
 
-			}
-			res.jsonp(result);
-		}
+      }
+      res.jsonp(result);
+    }
 
-	});
+  });
 };
 
 // tìm user trong group để chỉ định công việc
 module.exports.postSearchGroupUser = async function(req, res, next) {
-	var field = req.body.field;
-	var groupId = req.params.groupId;
+  var field = req.body.field;
+  var groupId = req.params.groupId;
 
-	var userId = req.signedCookies.userId;
-	// var userId = req.body.userId;
+  var userId = req.signedCookies.userId;
+  // var userId = req.body.userId;
 
-	if (groupId == "#null"){
-		var users = await User.find({_id: {$not: userId}});
-	}
-	else {
-		var partnersGroup = await User_Group.find({groupId: groupId, userId: {$ne: userId}});
-		partnerIds = partnersGroup.map((partnerGroup)=>{
-			return partnerGroup.userId;
-		});
-		
-		var users = await User.find({_id: {$in: partnerIds}});
-		users = users.map((user)=>{
-			return {
-				_id: user._id,
-				displayName: user.displayName,
-				email: user.email
-			}
-		})
-	}
-	
-	if (field != "#null")
-		users = filterUser(users, field);
-	res.send({
-		users: users
-	})
+  if (groupId == "#null"){
+    var users = await User.find({_id: {$not: userId}});
+  }
+  else {
+    var partnersGroup = await User_Group.find({groupId: groupId, userId: {$ne: userId}});
+    partnerIds = partnersGroup.map((partnerGroup)=>{
+      return partnerGroup.userId;
+    });
+    
+    var users = await User.find({_id: {$in: partnerIds}});
+    users = users.map((user)=>{
+      return {
+        _id: user._id,
+        displayName: user.displayName,
+        email: user.email
+      }
+    })
+  }
+  
+  if (field != "#null")
+    users = filterUser(users, field);
+  res.send({
+    users: users
+  })
 
 
 
--- dbBackUp/export_board.sh
+++
@@ -1,2 +0,0 @@
-mongoexport --uri="mongodb://localhost:27017/work_management"  --collection=board  --out=board.json
-pause
