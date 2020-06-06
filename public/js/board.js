535ef76cc8c05d7e1439655f4107d0da06442b3e
quangdat99 <dat21051999@gmail.com>
2020-06-06 17:20:41 +0700

    mm

--- public/js/board.js
+++ public/js/board.js
@@ -1,42 +1,47 @@
-      $(document).ready(function(){
-        $("#them1").click(function(){
-          $(".submit").fadeIn(300);
-          $("#them1").fadeOut(0);
-        });
-        $("#them2").click(function(){
-          $(".submit").fadeOut(0);
-          $("#them1").fadeIn(0);
-        });
+$(document).ready(function(){
+  $("#them1").click(function(){
+    $(".submit").fadeIn(300);
+    $("#them1").fadeOut(0);
+  });
+  $("#them2").click(function(){
+    $(".submit").fadeOut(0);
+    $("#them1").fadeIn(0);
+  });
 
-        
-        $(".list-add").click(function(){
-          $(".card-composer").fadeIn(0);
-          $(".list-add").fadeOut(0);
-          
-        });
 
-        $("#thoat").click(function(){
-          $(".card-composer").fadeOut(0);
-          $(".list-add").fadeIn(0);
-        });
-        
-        $(".u-bottom").click(function(){
-          $(".u-bottom").fadeOut(0);
-          $(".description-edit").fadeIn(0);
-        });
-        $("#xxx").click(function(){
-          $(".description-edit").fadeOut(0);
-          $(".u-bottom").fadeIn(0);
-        });
+  $(".list-add ").click(function(){
+    var id= $(this).attr("id").slice(3);
+    console.log(id);
+    $(" #card"+id).fadeIn(0);
+    $(" #add"+id).fadeOut(0);
+    
+  });
 
-        $("#themm").click(function(){
-          $("#themm").fadeOut(0);
-          $(".description-editt").fadeIn(0);
-        });
-        $("#xxy").click(function(){
-          $(".description-editt").fadeOut(0);
-          $("#themm").fadeIn(0);
-        });
-     
+  $(".thoatnhe").click(function(){
+    var id= $(this).attr("id").slice(5);
+    console.log(id);
+    $(" #card"+id).fadeOut(0);
+    $(" #add"+id).fadeIn(0);
+  });
+  
+  $(".u-bottom").click(function(){
+    $(".u-bottom").fadeOut(0);
+    $(".description-edit").fadeIn(0);
+  });
+  $("#xxx").click(function(){
+    $(".description-edit").fadeOut(0);
+    $(".u-bottom").fadeIn(0);
+  });
 
-      });
+  $("#themm").click(function(){
+    $("#themm").fadeOut(0);
+    $(".description-editt").fadeIn(0);
+  });
+  $("#xxy").click(function(){
+    $(".description-editt").fadeOut(0);
+    $("#themm").fadeIn(0);
+  });
+
+
+});
+
