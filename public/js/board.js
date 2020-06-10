57405b691a28a44de3c58553fcbf6526f8cb1597
quangdat99 <dat21051999@gmail.com>
2020-06-10 10:19:27 +0700

    status

--- public/js/board.js
+++ public/js/board.js
@@ -193,12 +193,184 @@ $(document).ready(function(){
 
 
   var poolTask = {};
+  var poolCheck ={};
+  var his_cardId;
+  
+  //create comment
+  $("input.comment-his").on("keydown", function search(e){
+    if (e.keyCode == 13){
+      var content = $(this).val();
+      var cardId = $(this).attr("cardId");
+      his_cardId=cardId;
+
+
+      $.ajax({
+        url: "/comment/create",
+        method: "POST",
+        dataType: "json",
+        data: {content: content, cardId: cardId},
+        success: function(data){
+          console.log(data);
+        }
+      })
+
+      $(this).val('');
+    }
+  })
+
+  //index card
+  
+  $("a.card-name").click(function(){
+    var cardId = $(this).attr('cardId');
+    var boardId = $(this).attr('boardId');
+    his_cardId = cardId;
+
+    $("#checklist"+cardId).html('');
+    $("#hd"+cardId).html('');
+    $.ajax({
+      url: "/card/index",
+      method: "POST",
+      dataType: "json",
+      data: {cardId: cardId},
+      success: function(data){
+        console.log(data);
+        data.card.tasks.forEach(task =>{
+          var status;
+          if (task.status==1) {status="checked"} else {status ="unchecked"};
+          if (task.user){
+            $("#checklist"+cardId).append('<div id="task'+task._id+'" class="window-module-title" style="display:block; overflow: hidden;"><div style="display:flex;"><span style="margin-right: 15px;padding: 5px;"><input type="checkbox" '+status+' class="checkbox-task" taskId="'+task._id+'"  id="checkbox'+task._id+'" style="zoom:2"></span><div class="editable checklist-title"><input class="title-task mod-card-back-title " dir="auto" style="overflow: hidden; overflow-wrap: break-word; height: 32px;width: 90%;color: #5e6c84; " taskId="'+task._id+'" value="'+task.title+'" ><a class="delete-task button subtle" taskId="'+task._id+'" taskTitle="'+task.title+'" href="#">X&oacute;a</a></div></div><div style="display: flex;margin-top: 10px;"><a class="button subtle appoint-member" taskId="'+task._id+'" id="chidinh'+task._id+'" href="#" style="float: left; margin-left: 53px;display: none; ">Chỉ định </a><input id="search'+task._id+'" taskId="'+task._id+'" type="text" class=" form-control search-member-group" style="width:200px; display:none; margin-left:53px;"><div  style="float: left;margin-left: 100px;color: #29a3a3; font-weight: 600;" id="result'+task._id+'">'+task.user.displayName+'<span  style="font-size: 14px;font-weight: 400;">&nbsp;đã được chỉ định thực hiện</span> </div></div><ul class="list-group result-search-group" id="result-'+task._id+'" taskId="'+task._id+'"></ul> </div>')
+          } else {
+            $("#checklist"+cardId).append('<div id="task'+task._id+'" class="window-module-title" style="display:block; overflow: hidden;"><div style="display:flex;"><span style="margin-right: 15px;padding: 5px;"><input type="checkbox" '+status+' class="checkbox-task" taskId="'+task._id+'"  id="checkbox'+task._id+'" style="zoom:2"></span><div class="editable checklist-title"><input class="title-task mod-card-back-title " dir="auto" style="overflow: hidden; overflow-wrap: break-word; height: 32px;width: 90%;color: #5e6c84; " taskId="'+task._id+'" value="'+task.title+'" ><a class="delete-task button subtle" taskId="'+task._id+'" taskTitle="'+task.title+'" href="#">X&oacute;a</a></div></div><div style="display: flex;margin-top: 10px;"><a class="button subtle appoint-member" taskId="'+task._id+'" id="chidinh'+task._id+'" href="#" style="float: left; margin-left: 53px; color: #777 ">Chỉ định thành viên thực hiện </a><input id="search'+task._id+'" taskId="'+task._id+'" type="text" class=" form-control search-member-group" style="width:200px; display:none; margin-left:53px;"><div id="result'+task._id+'" style="float: left;margin-left: 100px;color: #29a3a3;font-weight: 600;"  id="result'+task._id+'"></div></div><ul class="list-group result-search-group" id="result-'+task._id+'" taskId="'+task._id+'"></ul> </div>')
+          };
+          // if (task.status==1){
+          //   $('input#checkbox'+taskId).prop("checked", true);
+          // } else {
+          //   $('input#checkbox'+taskId).prop("checked", false);
+          // }
+        });
+        // update checkbox task
+        $("input.checkbox-task").click(function(){
+          var taskId =$(this).attr("taskId");
+          console.log(taskId);
+          var value = $(this).prop("checked");
+          console.log(value);
+          if (value===true){
+            var tonumber = $('span#comple'+cardId).text();
+            tonumber = Number(tonumber)+1;
+            $('span#comple'+cardId).text(tonumber);
+          } else{
+            var tonumber = $('span#comple'+cardId).text();
+            tonumber = Number(tonumber)-1;
+            $('span#comple'+cardId).text(tonumber);
+          }
+
+          if ($('span#comple'+cardId).text() === $('span#tasksCount'+cardId).text() && $('span#tasksCount'+cardId).text() !== '0'){
+            $("#cpl"+cardId).css({"background-color":"#61bd4f","color":"#fff","padding":"5px"});
+          } else {
+            $("#cpl"+cardId).css({"background-color":"","color":"","padding":""});
+          }
+
+          $.ajax({
+            url: "/task/toggleStatus",
+            method: "POST",
+            dataType: "json",
+            data: {taskId: taskId},
+            success: function(data){
+              console.log(data);
+            }
+          })
+        });
+
+        //appoint member
+        $("a.appoint-member").click(function(){
+          var taskId = $(this).attr("taskId");
+          $(this).fadeOut(0);
+          $("#search"+taskId).fadeIn(0).focus();
+        });
+        $("input.search-member-group").focusout(function(){
+          var taskId = $(this).attr("taskId");
+            $(this).fadeOut(0);
+            $("a#chidinh"+taskId).fadeIn(0);
+        });
+        $("input.search-member-group").on("keydown",function(e){
+          var taskId = $(this).attr("taskId");
+          if(e.keyCode ==13){
+            $(this).fadeOut(0);
+            $("a#chidinh"+taskId).fadeIn(0);
+          }
+        });
+        //search member group
+        $("input.search-member-group").keyup(function(search){
+          var value = $(this).val();
+          var taskId =$(this).attr("taskId");
+          $('#result-'+taskId).html('');
+          var groupId=($('#groupId').attr('groupId'));
+          $.ajax({
+            url: "/search/groupUser",
+            method: "POST",
+            dataType: "json",
+            data: {field: value, groupId: groupId},
+            success: function(data){
+              console.log(data);
+              data.forEach(user =>{
+                $('#result-'+taskId).append("<li userId="+ user._id+" taskId="+taskId+" class='list-group-item' style=' display:flex; '><img src='/image/group.png' style='height:40px;width:40px;' class='img-thumbnail'><div style='margin-left: 15px;'><div style='font-weight: 600;'>"+ user.displayName+ "</div><div style='font-size: 14px;color:#777;'>"+user.email+"</div></div></li>")
+              });
+
+            }
+          })
+
+          $('#result-'+taskId).on("click", "li", function(event){             
+            var taskId = $(this).attr("taskId");   
+            $('#result-'+taskId).html('');
+            $('input.search-member-group').val('');
+            var userId=($(this).attr('userId'));
+            var groupId=($('#groupId').attr('groupId'));
+
+            
+            $.ajax({
+              url: "/task/appoint",
+              method: "POST",
+              dataType: "json",
+              data: {appointedUserId: userId, taskId: taskId},
+              success: function(data){
+                console.log(data);
+                $('#result'+taskId).html('');
+                $('#result'+taskId).append(data.displayName+'<span  style="font-size: 14px;font-weight: 400;">&nbsp;đã được chỉ định thực hiện</span>');
+                $("#chidinh"+taskId).remove();
+              }
+            })
+            event.stopPropagation();
+          });
+        });
+
+
+        for(var i=data.card.histories.length-1; i>=0; i--){
+          var date = new Date(data.card.histories[i].timeCreated);
+          var day = date.getDate();
+          var month = date.getMonth()+1;
+          var year = date.getFullYear();
+          var hour = date.getHours();
+          var minute = date.getMinutes();
+          var second = date.getSeconds();
+
+          $("#hd"+cardId).append('<div class="js-menu-action-list"><div class="phenom"><div class="phenom-creator"><div class="member"><span class="member-initials"><i class="far fa-user" aria-hidden="true"> </i></span></div></div><div class="phenom-desc"><span class="inline-member">'+data.card.histories[i].header+'</span><div style="color:#777;">  &nbsp; '+data.card.histories[i].content +'</div></div><div class="phenom-meta">'+day+'-'+month+'-'+year+' '+hour+':'+minute+':'+second+'</div></div></div>')
+        }
+
+
+
+        addEventRenameTask();
+        addEventDeleteTask();
+      }
+    })
+
+  })
+
 
   //create Task
   $('button.themcongviec').click(function(){
     var cardId = $(this).attr('cardId');
     var title = $('input#themviec'+cardId).val();
-    console.log(title);
+    $('span#tasksCount'+cardId).text(Number($('span#tasksCount'+cardId).text())+1);
     console.log(cardId);
     $.ajax({
       url: "/task/create",
@@ -214,12 +386,28 @@ $(document).ready(function(){
         addEventRenameTask();
         addEventDeleteTask();
 
+
         // update checkbox task
         $("input.checkbox-task").click(function(){
           var taskId =$(this).attr("taskId");
           console.log(taskId);
-          // var value = $(this).prop("checked");
-          // console.log(value);
+          var value = $(this).prop("checked");
+          console.log(value);
+          if (value===true){
+            var tonumber = $('span#comple'+cardId).text();
+            tonumber = Number(tonumber)+1;
+            $('span#comple'+cardId).text(tonumber);
+          } else {
+            var tonumber = $('span#comple'+cardId).text();
+            tonumber = Number(tonumber)-1;
+            $('span#comple'+cardId).text(tonumber);
+          }
+          if ($('span#comple'+cardId).text() === $('span#tasksCount'+cardId).text() && $('span#tasksCount'+cardId).text() !== '0'){
+            $("#cpl"+cardId).css({"background-color":"#61bd4f","color":"#fff","padding":"5px"});
+          } else {
+            $("#cpl"+cardId).css({"background-color":"","color":"","padding":""});
+          }
+          
           $.ajax({
             url: "/task/toggleStatus",
             method: "POST",
@@ -318,162 +506,6 @@ $(document).ready(function(){
 
     $('input.vieccanlam').val('');
   })
-  var his_cardId;
-  
-  //create comment
-  $("input.comment-his").on("keydown", function search(e){
-    if (e.keyCode == 13){
-      var content = $(this).val();
-      var cardId = $(this).attr("cardId");
-      his_cardId=cardId;
-
-
-      $.ajax({
-        url: "/comment/create",
-        method: "POST",
-        dataType: "json",
-        data: {content: content, cardId: cardId},
-        success: function(data){
-          console.log(data);
-        }
-      })
-
-      $(this).val('');
-    }
-  })
-
-  //index card
-  
-  $("a.card-name").click(function(){
-    var cardId = $(this).attr('cardId');
-    var boardId = $(this).attr('boardId');
-    his_cardId = cardId;
-
-    $("#checklist"+cardId).html('');
-    $("#hd"+cardId).html('');
-    $.ajax({
-      url: "/card/index",
-      method: "POST",
-      dataType: "json",
-      data: {cardId: cardId},
-      success: function(data){
-        console.log(data);
-        data.card.tasks.forEach(task =>{
-          var status;
-          if (task.status==1) {status="checked"} else {status ="unchecked"};
-          if (task.user){
-            $("#checklist"+cardId).append('<div id="task'+task._id+'" class="window-module-title" style="display:block; overflow: hidden;"><div style="display:flex;"><span style="margin-right: 15px;padding: 5px;"><input type="checkbox" '+status+' class="checkbox-task" taskId="'+task._id+'"  id="checkbox'+task._id+'" style="zoom:2"></span><div class="editable checklist-title"><input class="title-task mod-card-back-title " dir="auto" style="overflow: hidden; overflow-wrap: break-word; height: 32px;width: 90%;color: #5e6c84; " taskId="'+task._id+'" value="'+task.title+'" ><a class="delete-task button subtle" taskId="'+task._id+'" taskTitle="'+task.title+'" href="#">X&oacute;a</a></div></div><div style="display: flex;margin-top: 10px;"><a class="button subtle appoint-member" taskId="'+task._id+'" id="chidinh'+task._id+'" href="#" style="float: left; margin-left: 53px;display: none; ">Chỉ định </a><input id="search'+task._id+'" taskId="'+task._id+'" type="text" class=" form-control search-member-group" style="width:200px; display:none; margin-left:53px;"><div  style="float: left;margin-left: 100px;color: #29a3a3; font-weight: 600;" id="result'+task._id+'">'+task.user.displayName+'<span  style="font-size: 14px;font-weight: 400;">&nbsp;đã được chỉ định thực hiện</span> </div></div><ul class="list-group result-search-group" id="result-'+task._id+'" taskId="'+task._id+'"></ul> </div>')
-          } else {
-            $("#checklist"+cardId).append('<div id="task'+task._id+'" class="window-module-title" style="display:block; overflow: hidden;"><div style="display:flex;"><span style="margin-right: 15px;padding: 5px;"><input type="checkbox" '+status+' class="checkbox-task" taskId="'+task._id+'"  id="checkbox'+task._id+'" style="zoom:2"></span><div class="editable checklist-title"><input class="title-task mod-card-back-title " dir="auto" style="overflow: hidden; overflow-wrap: break-word; height: 32px;width: 90%;color: #5e6c84; " taskId="'+task._id+'" value="'+task.title+'" ><a class="delete-task button subtle" taskId="'+task._id+'" taskTitle="'+task.title+'" href="#">X&oacute;a</a></div></div><div style="display: flex;margin-top: 10px;"><a class="button subtle appoint-member" taskId="'+task._id+'" id="chidinh'+task._id+'" href="#" style="float: left; margin-left: 53px; color: #777 ">Chỉ định thành viên thực hiện </a><input id="search'+task._id+'" taskId="'+task._id+'" type="text" class=" form-control search-member-group" style="width:200px; display:none; margin-left:53px;"><div id="result'+task._id+'" style="float: left;margin-left: 100px;color: #29a3a3;font-weight: 600;"  id="result'+task._id+'"></div></div><ul class="list-group result-search-group" id="result-'+task._id+'" taskId="'+task._id+'"></ul> </div>')
-          };
-          // if (task.status==1){
-          //   $('input#checkbox'+taskId).prop("checked", true);
-          // } else {
-          //   $('input#checkbox'+taskId).prop("checked", false);
-          // }
-        });
-        // update checkbox task
-        $("input.checkbox-task").click(function(){
-          var taskId =$(this).attr("taskId");
-          console.log(taskId);
-          // var value = $(this).prop("checked");
-          // console.log(value);
-          $.ajax({
-            url: "/task/toggleStatus",
-            method: "POST",
-            dataType: "json",
-            data: {taskId: taskId},
-            success: function(data){
-              console.log(data);
-            }
-          })
-        });
-
-        //appoint member
-        $("a.appoint-member").click(function(){
-          var taskId = $(this).attr("taskId");
-          $(this).fadeOut(0);
-          $("#search"+taskId).fadeIn(0).focus();
-        });
-        $("input.search-member-group").focusout(function(){
-          var taskId = $(this).attr("taskId");
-            $(this).fadeOut(0);
-            $("a#chidinh"+taskId).fadeIn(0);
-        });
-        $("input.search-member-group").on("keydown",function(e){
-          var taskId = $(this).attr("taskId");
-          if(e.keyCode ==13){
-            $(this).fadeOut(0);
-            $("a#chidinh"+taskId).fadeIn(0);
-          }
-        });
-        //search member group
-        $("input.search-member-group").keyup(function(search){
-          var value = $(this).val();
-          var taskId =$(this).attr("taskId");
-          $('#result-'+taskId).html('');
-          var groupId=($('#groupId').attr('groupId'));
-          $.ajax({
-            url: "/search/groupUser",
-            method: "POST",
-            dataType: "json",
-            data: {field: value, groupId: groupId},
-            success: function(data){
-              console.log(data);
-              data.forEach(user =>{
-                $('#result-'+taskId).append("<li userId="+ user._id+" taskId="+taskId+" class='list-group-item' style=' display:flex; '><img src='/image/group.png' style='height:40px;width:40px;' class='img-thumbnail'><div style='margin-left: 15px;'><div style='font-weight: 600;'>"+ user.displayName+ "</div><div style='font-size: 14px;color:#777;'>"+user.email+"</div></div></li>")
-              });
-
-            }
-          })
-
-          $('#result-'+taskId).on("click", "li", function(event){             
-            var taskId = $(this).attr("taskId");   
-            $('#result-'+taskId).html('');
-            $('input.search-member-group').val('');
-            var userId=($(this).attr('userId'));
-            var groupId=($('#groupId').attr('groupId'));
-
-            
-            $.ajax({
-              url: "/task/appoint",
-              method: "POST",
-              dataType: "json",
-              data: {appointedUserId: userId, taskId: taskId},
-              success: function(data){
-                console.log(data);
-                $('#result'+taskId).html('');
-                $('#result'+taskId).append(data.displayName+'<span  style="font-size: 14px;font-weight: 400;">&nbsp;đã được chỉ định thực hiện</span>');
-                $("#chidinh"+taskId).remove();
-              }
-            })
-            event.stopPropagation();
-          });
-        });
-
-
-        for(var i=data.card.histories.length-1; i>=0; i--){
-          var date = new Date(data.card.histories[i].timeCreated);
-          var day = date.getDate();
-          var month = date.getMonth()+1;
-          var year = date.getFullYear();
-          var hour = date.getHours();
-          var minute = date.getMinutes();
-          var second = date.getSeconds();
-
-          $("#hd"+cardId).append('<div class="js-menu-action-list"><div class="phenom"><div class="phenom-creator"><div class="member"><span class="member-initials"><i class="far fa-user" aria-hidden="true"> </i></span></div></div><div class="phenom-desc"><span class="inline-member">'+data.card.histories[i].header+'</span><div style="color:#777;">  &nbsp; '+data.card.histories[i].content +'</div></div><div class="phenom-meta">'+day+'-'+month+'-'+year+' '+hour+':'+minute+':'+second+'</div></div></div>')
-        }
-
-
-
-        addEventRenameTask();
-        addEventDeleteTask();
-      }
-    })
-
-  })
-
-
 
 
   //rename card
